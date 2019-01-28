fd26e647eb18a68c0c8a88df775442f2d8e66bc0
Setia Bhakti <joebhakti513@gmail.com>
2019-01-28 13:29:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?
-echo 'Hai Nakal Tapi Tampan :)';
+<?php
+
+$namafile = "data4.txt";
+$handle = fopen ($namafile, "w");
+
+if (!$handle) {
+	echo "<b>Error 404</b>";
+} else {
+	$isi = "Pap Skdipap";
+	fwrite ($handle, $isi);
+	echo "<b>Berhasil :)</b>"; 
+}

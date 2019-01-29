0569c69b0c2731b29be9c7f66af55caf8f9adfde
Setia Bhakti <joebhakti513@gmail.com>
2019-01-29 10:38:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
-
-$namafile = "data4.txt";
+$namafile = "cron.xml";
 $handle = fopen ($namafile, "w");
 
-if (!$handle) {
-	echo "<b>Error 404</b>";
-} else {
-	$isi = "Pap Skdipap";
+if ($handle) {
+	$op = '<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:nyt="http://www.nytimes.com/namespaces/rss/2.0" version="2.0">'."\n<channel>\n<title>RSS testo</title>
+	";
+	
+	$ed = "\n</channel>\n</rss>";
+	$isi = $op. "<item>\n"."\n</item>" .$ed;
 	fwrite ($handle, $isi);
-	echo "<b>Berhasil :)</b>"; 
-}
+	echo '<b>Upadate: '.date("H:i/d-m").'</b>'; 
+} else {
+	echo 'error statmenect';}

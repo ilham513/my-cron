766c046e8c8f5edf9c668116fb1378130da7c90e
Setia Bhakti <joebhakti513@gmail.com>
2019-01-30 13:28:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
 <?php
+
+$opn = '<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:nyt="http://www.nytimes.com/namespaces/rss/2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" version="2.0">'.
+"\n<channel>\n<title>RSS testo</title>";
+$end = "\n</channel>\n</rss>";
+
+$file = simplexml_load_file("Business.xml");
+foreach ($file as $value) {
+	for($i=0;$i<5;$i++){
+		$item = $value-> item[$i];
+		
+		$jdl = $item -> title;
+		$lnk = $item -> link;
+		$des = $item -> description;
+		$tgl = $item -> pubDate;
+		$img = $item -> children('media', TRUE) -> content-> attributes() -> url;
+
+		$jdl = '<title>'.$jdl.'</title>';
+		$lnk = '<link><![CDATA['.$lnk.']]></link>';
+		$des = '<description>'.$des.'</description>';
+		$lbd[] = '<lastBuildDate>'.$tgl.'</lastBuildDate>';
+		$tgl = '<pubDate>'.$tgl.'</pubDate>';
+		$img = '<content:encoded><![CDATA[<img src="'.$img.'" />]]></content:encoded>';
+		
+		$all = $jdl."\n".$lnk."\n".$des."\n".$tgl."\n".$img;
+		$isi[] = $all;
+		echo $all;
+		echo "<hr/>";
+	}
+}
+
 $namafile = "cron.xml";
 $handle = fopen ($namafile, "w");
 
-if ($handle) {
-	$op = '<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:nyt="http://www.nytimes.com/namespaces/rss/2.0" version="2.0">'."\n<channel>\n<title>RSS testo</title>
-	";
-	
-	$ed = "\n</channel>\n</rss>";
-	$isi = $op. "<item>\n"."\n</item>" .$ed;
-//	fwrite ($handle, $isi);
-	fwrite ($handle, 'RE: Write!!!');
-	echo '<b>Upadate: '.date("H:i/d-m").'</b>'; 
-} else {
-	echo 'error statmenect';}
+foreach($isi as $isi){
+	$myitem[] = "<item>\n" .$isi."\n</item>";
+}
+
+$myitem = implode("\n",$myitem);
+$myitem = $opn."\n".$lbd[0]."\n".$myitem."\n".$end;
+fwrite ($handle, $myitem);
+echo '<b>Berhasil Upadate: '.date("H:i/d-m").'</b>'; 
